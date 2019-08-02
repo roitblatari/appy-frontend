09cc84956159bbdf4ed782fb88d9076f70233c17
roitblatari <roitblatari@gmail.com>
2019-08-02 11:25:04 -0400

    created store for redux

--- src/index.js
+++ src/index.js
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-// import { createStore, applyMiddleware, compose } from 'redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import usersReducer from './reducers/users';
-// import users from './reducers/users';
+import store from './store.js';
 // import { Provider ,  connect } from 'react-redux';
-
-const reducer = combineReducers({
-	user: usersReducer
-});
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
