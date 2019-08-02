cbab00ab77901ba4cf7cc9bebdff854a1e56556d
roitblatari <roitblatari@gmail.com>
2019-08-02 09:48:34 -0400

    Mock reducer

---
+++ src/reducers/users.js
@@ -0,0 +1,16 @@
+export default (state = [], action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
+
+// export default (state = [], action) => {
+// 	switch (action.type) {
+// 		case users:
+// 			return [ ...state, action ];
+
+// 		default:
+// 			return state;
+// 	}
+// };
