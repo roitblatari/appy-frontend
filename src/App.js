c81567b8555d681dab02d5a5630e50928c66503b
roitblatari <roitblatari@gmail.com>
2019-08-05 20:09:09 -0400

    return the login component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-
+// import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login';
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-					<h2>I'm the App Component</h2>
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
+			// <div className="App">
+			// 	<div className="App-header">
+			//<h2>I'm the App Component</h2>
+			<Login />
+			// 	</div>
+			// 	<p className="App-intro">
+			// 		To get started, edit <code>src/App.js</code> and save to reload.
+			// 	</p>
+			// </div>
 		);
 	}
 }
