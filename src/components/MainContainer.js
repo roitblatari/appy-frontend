0e9825e898ef1dec4ed2d70826259663888bfbdc
roitblatari <roitblatari@gmail.com>
2019-09-10 10:43:56 -0400

    main con.

---
+++ src/components/MainContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainContainer = () => {
+	return (
+		<div className="mainContainer">
+			Did you know that I am the <strong>MainContainer</strong> component?
+		</div>
+	);
+};
+
+export default MainContainer;
