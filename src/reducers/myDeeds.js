78ee40079f26ce383816e1457f4243cfd8a92450
roitblatari <roitblatari@gmail.com>
2019-09-11 11:44:21 -0400

    created myDeeds reducer

---
+++ src/reducers/myDeeds.js
@@ -0,0 +1,18 @@
+// const initialState = {
+// 	myDeeds: []
+// };
+
+export default (state = [], action) => {
+	switch (action.type) {
+		case 'SET_MY_DEEDS':
+			return {
+				...state,
+				myDeeds: action.deeds
+			};
+		// return action.deeds;
+		// case 'CLEAR_CURRENT_USER':
+		// 	return initialState;
+		default:
+			return state;
+	}
+};
