9d0b5965ba1c07534f3d13326dc1a61a760a4830
roitblatari <roitblatari@gmail.com>
2019-09-11 11:42:45 -0400

    import myDeeds

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import MyDeeds from './MyDeeds';
 
 const MainContainer = () => {
 	return (
 		<div className="mainContainer">
 			Did you know that I am the <strong>MainContainer</strong> component?
+			<MyDeeds />
 		</div>
 	);
 };
