c2826fc402e66daa9d3afff66f62c0c417a86bd2
roitblatari <roitblatari@gmail.com>
2019-10-29 11:29:45 -0400

    BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store.js';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import { Provider ,  connect } from 'react-redux';
 
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<Router>
+			<App />
+		</Router>
 	</Provider>,
 	document.getElementById('root')
 );
