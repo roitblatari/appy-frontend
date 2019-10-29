aa7f5e1343dd4efd2b35d1a0f2a3b0b4d581b262
roitblatari <roitblatari@gmail.com>
2019-10-29 11:40:56 -0400

    reflector

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,65 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import Login from './Login';
+import { connect } from 'react-redux';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+// import { Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+
+// import LoginForm from './LoginForm';
 import Logout from './Logout';
-import Signup from './Signup';
+// import Signup from './Signup';
+// import { logout } from '../actions/currentUser';
 
 const NavBar = ({ currentUser }) => {
-	// debugger;
 	return (
-		<div className="nav">
-			{/* {currentUser ? <strong> Welcome {currentUser.data.attributes.username} </strong> : ''} */}
-			<br /> <br />
-			{/* {currentUser ? <Logout /> : <Login />} */}
-			<Login />
-			<Logout />
-			<Signup />
-		</div>
+		// <div className="nav">
+		// 	<br /> <br />
+		<Navbar bg="dark" variant="dark">
+			<Navbar.Brand>
+				{/* <Nav.Link href="/login">Login</Nav.Link> */}
+				{currentUser ? (
+					<strong> Welcome {currentUser.data.attributes.username} </strong>
+				) : (
+					<Nav.Link href="/"> Appy</Nav.Link>
+				)}
+			</Navbar.Brand>
+			{!currentUser ? (
+				<div className={'currentUserIsTrue'}>
+					<Nav className="mr-auto">
+						{/* <Nav.Link href="/login">Login</Nav.Link> */}
+						<Nav.Link>
+							<Link to="/login">Log In</Link>
+						</Nav.Link>
+						<Nav.Link>
+							<Link to="/signup">Signup </Link>
+						</Nav.Link>
+						{/* <Nav.Link href="/login">Login</Nav.Link> */}
+					</Nav>
+				</div>
+			) : (
+				<Nav>
+					{' '}
+					<Nav.Link>
+						<Logout />
+					</Nav.Link>
+				</Nav>
+			)}
+
+			{/* <Nav>
+				{currentUser ? (
+					<Nav.Link>
+						<Logout />
+					
+					</Nav.Link>
+				) : null}
+			</Nav> */}
+			{/* <Form inline>
+					<FormControl type="text" placeholder="Search" className="mr-sm-2" />
+					<Button variant="outline-info">Search</Button>
+				</Form> */}
+		</Navbar>
 	);
 };
 
