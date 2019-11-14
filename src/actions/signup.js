c6f9be34480c7613a5f8f32dfb4ce3eaf9727034
roitblatari <roitblatari@gmail.com>
2019-11-14 12:12:39 -0500

    not needed

--- src/actions/signup.js
+++ src/actions/signup.js
@@ -1,14 +1,14 @@
-export const updateSignupForm = (formData) => {
-	// console.log(formData);
-	return {
-		type: 'UPDATE_SIGNUP_FORM',
-		formData
-	};
-};
+// export const updateSignupForm = (formData) => {
+// 	// console.log(formData);
+// 	return {
+// 		type: 'UPDATE_SIGNUP_FORM',
+// 		formData
+// 	};
+// };
 
-export const resetSignupForm = () => {
-	// console.log(formData);
-	return {
-		type: 'RESET_CURRENT_USER'
-	};
-};
+// export const resetSignupForm = () => {
+// 	// console.log(formData);
+// 	return {
+// 		type: 'RESET_CURRENT_USER'
+// 	};
+// };
