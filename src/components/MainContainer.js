09bf1dc9bbefe8061bbbb806c24675df152da840
roitblatari <roitblatari@gmail.com>
2019-11-14 12:11:24 -0500

    main container content

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import MyDeeds from './MyDeeds';
+import DeedsContainer from './DeedsContainer';
 
-const MainContainer = () => {
+import Jumbotron from 'react-bootstrap/Jumbotron';
+// import SidePanel from 'react-bootstrap/SidePanel';
+
+import Container from 'react-bootstrap/Container';
+import { connect } from 'react-redux';
+// import DeedForm from './DeedForm';
+
+const MainContainer = ({ currentUser }) => {
 	return (
+		// <div className="mainContainer">
 		<div className="mainContainer">
-			Did you know that I am the <strong>MainContainer</strong> component?
-			<MyDeeds />
+			{/* <Jumbotron>{currentUser.username}</Jumbotron> */}
+			{/* {currentUser ? <MyDeeds /> : null
+			// Did you know that I am the <strong>MainContainer</strong> component?
+			}DeedsContainer
+			{currentUser ? <DeedForm /> : null
+			// Did you know that I am the <strong>MainContainer</strong> component?
+			} */}
+			{currentUser ? (
+				<Jumbotron fluid>
+					{/* <SidePanel> */}
+					{/* <ListGroup className="flex-column" md={{ span: 4, offset: 4 }}> */}
+
+					{/* </SidePanel> */}
+					<Container>
+						<DeedsContainer />
+					</Container>
+				</Jumbotron>
+			) : null}
 		</div>
 	);
 };
 
-export default MainContainer;
+const mapStateToProps = ({ currentUser }) => {
+	return {
+		currentUser
+	};
+};
+
+export default connect(mapStateToProps)(MainContainer);
