476518a76106da8af8cef341d0a8b3b03a9d3747
roitblatari <roitblatari@gmail.com>
2019-11-26 14:18:13 -0500

    cleanUp

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,54 +4,33 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-// import { Route } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
 
-// import LoginForm from './LoginForm';
 import Logout from './Logout';
-// import { ButtonToolbar } from 'react-bootstrap';
-// import Signup from './Signup';
-// import { logout } from '../actions/currentUser';
 
 const NavBar = ({ currentUser, history }) => {
 	return (
-		// <div className="nav">
-		// 	<br /> <br />
 		<Navbar bg="dark" variant="dark">
 			<Navbar.Brand>
-				{/* <Nav.Link href="/login">Login</Nav.Link> */}
-				{currentUser ? (
-					<div>
-						{/* <Nav.Link href="/"> */}
-						Welcome {currentUser.data.attributes.username}
-						<style hidden>.</style>
-						{/* </Nav.Link> */}
-						<Link to="/deedForm" userId={currentUser.data.attributes.id}>
-							{' '}
-							Create Deed
-						</Link>
-						{/* </Nav.Link> */}
-					</div>
-				) : (
-					<Nav.Link href="/"> Appy</Nav.Link>
-				)}
+				<Nav.Link href="/"> Appy</Nav.Link>
 			</Navbar.Brand>
+			{currentUser ? (
+				<React.Fragment>
+					<Nav style={{ color: 'white' }}>Welcome {currentUser.data.attributes.username}</Nav>
+
+					<Nav.Link href="/deedForm" userId={currentUser.data.attributes.id}>
+						Create Deed
+					</Nav.Link>
+				</React.Fragment>
+			) : null}
 			{!currentUser ? (
-				<div className={'currentUserIsTrue  float-xl-right'}>
+				<div className={'currentUserIsTrue ml-auto'}>
 					<Nav className="float-xl-right">
-						<Nav.Link>
-							<Link to="/login">Log In</Link>
-						</Nav.Link>
-						<Nav.Link>
-							<Link to="/signup">Signup </Link>
-						</Nav.Link>
+						<Nav.Link href="/login">Log In</Nav.Link>
+						<Nav.Link href="/signup">Signup </Nav.Link>
 					</Nav>
 				</div>
 			) : (
-				<Nav>
-					{/* <Nav.Link>
-						<Link to="/deedForm">DeedForm</Link>
-					</Nav.Link> */}
+				<Nav className="ml-auto">
 					<Nav.Link>
 						<Logout history={history} />
 					</Nav.Link>
