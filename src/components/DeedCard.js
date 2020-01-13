1ab31a753d2ec5589c54d194c37336faded6f9cb
roitblatari <roitblatari@gmail.com>
2020-01-13 13:55:46 -0500

    Live coding/Like button /Pure react  /Assessment

--- src/components/DeedCard.js
+++ src/components/DeedCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React , { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Card from 'react-bootstrap/Card';
@@ -6,30 +6,52 @@ import Button from 'react-bootstrap/Button';
 
 import { deleteDeed } from '../actions/myDeeds';
 
-const DeedCard = (props) => {
+class DeedCard extends Component {
+	state = {
+		like: false
+	}
 
-	const title = props.deed.attributes.title;
-	const content = props.deed.attributes.content;
 
-	const handleClick = () => {
-		props.deleteDeed(props.deed.attributes.id);
+
+ handleLikeClick = ()=> {
+	 if (this.state.like) {
+		 this.setState({like: false})
+	 } else {
+		 this.setState({like: true})
+	 }
+	
+}
+
+ handleClick = () => {
+		this.props.deleteDeed(this.props.deed.attributes.id);
 	};
 
-	return (
-		<div>
-			<Card style={{ width: '18rem' }}>
-				<Card.Body>
-					<Card.Title>Deed # {props.deed.attributes.id}</Card.Title>
-					<Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
-					<Card.Text>{content}</Card.Text>
-					<Button onClick={handleClick} variant="primary">
-						Delete
-					</Button>
-
-				</Card.Body>
-			</Card>
-		</div>
-	);
+	render() {
+		const title = this.props.deed.attributes.title;
+		const content = this.props.deed.attributes.content;
+		const like = this.state.like
+		return (
+			<div>
+				<Card style={{ width: '18rem' }}>
+					<Card.Body>
+		<div >{this.state.like ?"Liked" : " "} </div>
+						<Card.Title>Deed # {this.props.deed.attributes.id}</Card.Title>
+						<Card.Subtitle className="mb-2 text-muted">{title}</Card.Subtitle>
+						<Card.Text>{content}</Card.Text>
+		    <Button  onClick={this.handleLikeClick} variant="primary">
+							{!this.state.like ? "Like" : "Unlike"}
+						</Button>
+	
+						<Button onClick={this.handleClick} variant="primary">
+							Delete
+						</Button>
+	
+					</Card.Body>
+				</Card>
+			</div>
+		);
+	
+	}
 };
 
 export default connect(null, { deleteDeed })(DeedCard);
